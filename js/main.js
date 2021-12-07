43f565304ac77a6cacaf02428b848b959ee91611
safatas <302710276@student.rocmondriaan.nl>
2021-12-07 21:30:03 +0100

    dit is de index

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log =('file loaded')
+
+alert ('Welkom op www.SafaTas.nl')
+
+const result = confirm ( 'Wilt u cookies accepteren ?');
+if (result) {
+    alert ('U heeft de cookies geaccepteerd.')
+} else {
+    alert ( 'U heeft de cookies niet geaccepteerd.')
+}
+alert ('Nu gaat u de portfolio pagina van Safa Tas zien.')
+
+const myName = ('Safa Tas')
+
+let  total;
+total = 0;
+total = total + 50;
