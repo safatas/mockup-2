66dce32f76671b9a52cb7d22070b4e0bb15d19a5
safatas <302710276@student.rocmondriaan.nl>
2021-12-13 09:57:21 +0100

    werken aan contact pagina

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-console.log =('file loaded')
+// console.log =('file loaded')
 
-alert ('Welkom op www.SafaTas.nl')
+// alert ('Welkom op www.SafaTas.nl')
 
-const result = confirm ( 'Wilt u cookies accepteren ?');
-if (result) {
-    alert ('U heeft de cookies geaccepteerd.')
-} else {
-    alert ( 'U heeft de cookies niet geaccepteerd.')
-}
-alert ('Nu gaat u de portfolio pagina van Safa Tas zien.')
+// const result = confirm ( 'Wilt u cookies accepteren ?');
+// if (result) {
+//     alert ('U heeft de cookies geaccepteerd.')
+// } else {
+//     alert ( 'U heeft de cookies niet geaccepteerd.')
+// }
+// alert ('Nu gaat u de portfolio pagina van Safa Tas zien.')
 
-const myName = ('Safa Tas')
+// const myName = ('Safa Tas')
 
-let  total;
-total = 0;
-total = total + 50;
+// let  total;
+// total = 0;
+// total = total + 50;
